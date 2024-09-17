85234c823e22972f48f9323d7819f0036a120e58
TeaLizard <144549537+TeaLizard@users.noreply.github.com>
2024-09-17 18:10:59 -0400

    RegularExpressionMatchingSolution

---
+++ LeetCode_RegularExpressionMatching/LeetCode_RegularExpressionMatching/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+
+using System.Text.RegularExpressions;
+
+public class Solution
+{
+    public static void Main()
+    {
+        Console.WriteLine(IsMatch("aa", ".*"));
+    }
+    public static bool IsMatch(string s, string p)
+    {
+        string newPattern = @"^";
+        foreach (char c in p)
+        {
+            if (c == '.')
+            {
+                newPattern += "[A-z]";
+            }
+            else
+            {
+                newPattern += c;
+            }
+        }
+        newPattern += "$";
+        Regex regex = new Regex(@newPattern);
+
+        if (regex.IsMatch(s))
+        {
+            return true;
+        }
+        return false;
+    }
+}
